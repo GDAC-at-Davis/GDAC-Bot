e8a73287c919091bb610661774ee8329099ed61c
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-03-26 12:52:33 -0700

    Fix: added backup to gitignore | Feat: added officerInRoom field in GuildInfo

--- src/guild-info.ts
+++ src/guild-info.ts
@@ -1,4 +1,4 @@
-import { Guild, Snowflake } from 'discord.js';
+import { Guild, GuildMember, Snowflake } from 'discord.js';
 import { client } from './client.js';
 import { backupServerSettings } from './backup.js';
 
@@ -6,11 +6,13 @@ class GuildInfo {
     public readonly guild: Guild;
     private officerRole: Snowflake | null;
     private roomName: string | null;
+    private officersInRoom: GuildMember[] = [];
 
-    constructor(guild: Guild) {
+    constructor(guild: Guild, officerRole?: Snowflake | null, roomName?: string | null, officersInRoom?: GuildMember[]) {
         this.guild = guild;
-        this.officerRole = null;
-        this.roomName = null;
+        this.officerRole = officerRole ?? null;
+        this.roomName = roomName ?? null;
+        this.officersInRoom = officersInRoom ?? [];
     }
 
     public getOfficerRole(): Snowflake | null {
@@ -31,20 +33,35 @@ class GuildInfo {
         backupServerSettings(this.guild.id);
     }
 
+    public getOfficersInRoom(): GuildMember[] {
+        return this.officersInRoom;
+    }
+
+    public addOfficerToRoom(member: GuildMember): void {
+        this.officersInRoom.push(member);
+    }
+
+    public removeOfficerFromRoom(member: GuildMember): void {
+        this.officersInRoom = this.officersInRoom.filter(officer => officer.id !== member.id);
+    }
+
+    
     public toJSON(): GuildInfoBackup {
         return {
             guildId: this.guild.id,
             officerRole: this.officerRole,
-            roomName: this.roomName
+            roomName: this.roomName,
+            officersInRoom: this.officersInRoom.map(officer => officer.id)
         };
     }
 
     public static async fromJSON(data: GuildInfoBackup): Promise<GuildInfo> {
         const guildId = data.guildId;
         const guild = await client.guilds.fetch(guildId);
-        const server = new GuildInfo(guild);
-        server.setOfficerRole(data.officerRole);
-        server.setRoomName(data.roomName);
+        const officersInRoom = await Promise.all(data.officersInRoom.map(async officerId => {
+            return await guild.members.fetch(officerId);
+        }));
+        const server = new GuildInfo(guild, data.officerRole, data.roomName, officersInRoom);
         return server;
     }
 }
@@ -53,6 +70,7 @@ type GuildInfoBackup = {
     guildId: Snowflake;
     officerRole: Snowflake | null;
     roomName: string | null;
+    officersInRoom: Snowflake[];
 };
 
 export { GuildInfo, GuildInfoBackup };
