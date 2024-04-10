c2422e2f24b5c233ffdc0ee118c4954a6cc9bc30
Arthur Gan <arthurgan29@gmail.com>
2024-04-10 01:26:14 -0700

    whoops

--- setup-repo.bash
+++
@@ -1,5 +0,0 @@
-cp -n creds/bot_creds_template.json creds/bot_creds.json
-echo "Bot creds template copied to creds/bot_creds.json, please fill in the keys and such"
-nvm install --lts
-npm install
-npm install pm2 -g
