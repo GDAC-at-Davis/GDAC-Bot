b2a0f8b649f39fb0350ddabd0f40916fc637703a
Arthur Gan <arthurgan29@gmail.com>
2024-04-11 17:39:51 -0700

    cleaned up setup script

--- setup-repo.sh
+++ setup-repo.sh
@@ -2,8 +2,4 @@ mkdir creds
 cp -n bot_creds_template.json creds/bot_creds_prod.json
 cp -n bot_creds_template.json creds/bot_creds_dev.json
 echo "Bot creds template copied to creds/bot_creds.json, please fill in the keys and such"
-nvm install --lts
-npm install
-npm install pm2 -g
-
 mkdir backups/
